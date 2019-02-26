d68f2a9ce64d39d93b81e7d6fc746df6a25e378a
Mikhail <43931801+MikhailOnuchin@users.noreply.github.com>
2019-02-26 22:13:34 +0300

    started lesson logic

--- bowlingvo/main/static/main/test.js
+++ bowlingvo/main/static/main/test.js
@@ -6,28 +6,57 @@ function show_div(id) {
 	document.getElementById(id).style.display = 'block';
 }
 
-function hide_show(id) {
-	var my_div_style = document.getElementById(id).style;
-	if (my_div_style.display != 'none') {
-		hide_div(id);
-	} else {
-		show_div(id);
+
+function LessonLogic() {
+	
+	var self = this;
+	
+	var task_node = document.getElementById('all-tasks');
+	
+	var my_tasks = [].slice.call(document.getElementsByClassName('task'));
+	
+	var show_queue = [];
+	
+	for (var i = 1; i <= my_tasks.length; i++) {
+		show_queue.push(i);
+	}
+	
+	self.makeTasksList = function() {
+		console.log(my_tasks);
+	}
+	
+	self.checkTask = function() {
+		var cur_id = show_queue[0];
+		var task = document.getElementById('task' + cur_id);
+		
+		var answer = task.getAttribute('answer');
+		
+		var user_input = task.getElementsByTagName('input');
+		
+		if (user_input.length == 1) {
+			var user_answer = user_input[0].value;
+		} else {
+			for (var inp in user_input) {
+				if (inp.checked) {
+					var user_answer = inp.value;
+					break;
+				}
+			}
+			if (typeof(user_answer) == "undefined") {
+				var user_answer = false;
+			}
+		}
+		
+		if (user_input == answer) {
+			alert('Верно');
+		} else {
+			alert('Неверно');
+		}
 	}
 }
 
-function inverse_color(id) {
-    el = document.getElementById(id);
-    if (el.className == 'red') {
-        el.className = 'green';
-    } else {
-        el.className = 'red';
-    }
-}
 
-function switch_color() {
-    inverse_color('task1');
-    inverse_color('task2');
-}
+var lesson = new LessonLogic();
 
 
 function show_lex() {
